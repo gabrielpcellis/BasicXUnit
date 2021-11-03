e063dc838884b622830b7024c5d16453385d0bf1
Gabriel Porcellis <gabrielporcellis@gmail.com>
2021-11-03 16:33:14 -0300

    A basic class Calculations + XUnit project

---
+++ Calculations/Calculations/Calculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    class Calculation
+    {
+        public static double Addition(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+        public static double Subtraction(double number1, double number2)
+        {
+            return (number1 - number2);
+        }
+        public static double Multiplication(double number1, double number2)
+        {
+            return (number1 * number2);
+        }
+        public static double Division(double number1, double number2)
+        {
+            return (number1 / number2);
+        }
+        public static bool IsEvenNumber(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
